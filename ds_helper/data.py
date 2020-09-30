b5b7f11aca0618d22b0962e8e50e2d22b62aa0df
Paulo Souza <pe_fs@hotmail.com>
2020-09-30 14:55:23 -0300

    Updated get_prj_path and reduce_memory functions from data module.

--- ds_helper/data.py
+++ ds_helper/data.py
@@ -39,7 +39,7 @@ def pickle_dump(obj, path):
         dump(obj, f)
 
 
-def reduce_memory(df):
+def reduce_memory(big_df):
     """
     Memory Consumption Reduction
 
@@ -48,46 +48,49 @@ def reduce_memory(df):
 
     Parameters
     ----------
-    df : pandas DataFrame
+    big_df : pandas DataFrame
 
     Returns
     -------
     pandas DataFrame
         Memory-optimized pandas object
     """
+    df = big_df.copy()
     start_mem = df.memory_usage().sum() / 1024**2
     print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
 
     for col in df.columns:
         col_type = df[col].dtype
-
-        if col_type != object:
-            c_min = df[col].min()
-            c_max = df[col].max()
-            if str(col_type)[:3] == 'int':
-                if c_min > np.iinfo(np.int8).min \
-                        and c_max < np.iinfo(np.int8).max:
-                    df[col] = df[col].astype(np.int8)
-                elif c_min > np.iinfo(np.int16).min \
-                        and c_max < np.iinfo(np.int16).max:
-                    df[col] = df[col].astype(np.int16)
-                elif c_min > np.iinfo(np.int32).min \
-                        and c_max < np.iinfo(np.int32).max:
-                    df[col] = df[col].astype(np.int32)
-                elif c_min > np.iinfo(np.int64).min \
-                        and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int64)
-            else:
-                if c_min > np.finfo(np.float16).min \
-                        and c_max < np.finfo(np.float16).max:
-                    df[col] = df[col].astype(np.float16)
-                elif c_min > np.finfo(np.float32).min \
-                        and c_max < np.finfo(np.float32).max:
-                    df[col] = df[col].astype(np.float32)
-                else:
-                    df[col] = df[col].astype(np.float64)
+        if col_type == 'datetime64[ns]' or col_type == 'timedelta64[ns]':
+            pass
         else:
-            df[col] = df[col].astype('category')
+            if col_type != object:
+                c_min = df[col].min()
+                c_max = df[col].max()
+                if str(col_type)[:3] == 'int':
+                    if c_min > np.iinfo(np.int8).min \
+                            and c_max < np.iinfo(np.int8).max:
+                        df[col] = df[col].astype(np.int8)
+                    elif c_min > np.iinfo(np.int16).min \
+                            and c_max < np.iinfo(np.int16).max:
+                        df[col] = df[col].astype(np.int16)
+                    elif c_min > np.iinfo(np.int32).min \
+                            and c_max < np.iinfo(np.int32).max:
+                        df[col] = df[col].astype(np.int32)
+                    elif c_min > np.iinfo(np.int64).min \
+                            and c_max < np.iinfo(np.int64).max:
+                        df[col] = df[col].astype(np.int64)
+                else:
+                    if c_min > np.finfo(np.float16).min \
+                            and c_max < np.finfo(np.float16).max:
+                        df[col] = df[col].astype(np.float16)
+                    elif c_min > np.finfo(np.float32).min \
+                            and c_max < np.finfo(np.float32).max:
+                        df[col] = df[col].astype(np.float32)
+                    else:
+                        df[col] = df[col].astype(np.float64)
+            else:
+                df[col] = df[col].astype('category')
     end_mem = df.memory_usage().sum() / 1024**2
     print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
     print('Decreased by {:.1f}%'
@@ -107,7 +110,12 @@ def get_prj_path(proj_name):
         String describing the project root folder name.
     """
     curr = os.getcwd().split('/')
-    while curr[-1] != proj_name:
-        curr.pop()
-    path = '/'.join(curr)
+    path = []
+    for folder in curr:
+        if folder == proj_name:
+            path.append(folder)
+            break
+        else:
+            path.append(folder)
+    path = '/'.join(path)
     return path
